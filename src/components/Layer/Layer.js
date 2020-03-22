3ded49ea6f74d8f735d588dfc54245c92e544b48
brennop <brennopcordeiro@gmail.com>
2020-03-22 11:01:52 -0300

    ⭐ Add collapse button for layers

--- src/components/Layer/Layer.js
+++ src/components/Layer/Layer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../Button";
 import {
   Wrapper,
@@ -11,6 +11,7 @@ import { useDispatch, useSelector } from "react-redux";
 const Layer = ({ node }) => {
   const dispatch = useDispatch();
   const selected = useSelector(store => store.selected);
+  const [collapsed, setCollapsed] = useState(false);
 
   function addLayer(event) {
     event.stopPropagation();
@@ -30,6 +31,15 @@ const Layer = ({ node }) => {
   return (
     <Wrapper onClick={selectLayer}>
       <Main color={node.color} selected={selected?.id === node.id}>
+        {node.children.length !== 0 && (
+          <Button
+            size="20px"
+            margin="0 2px"
+            onClick={() => setCollapsed(c => !c)}
+          >
+            {collapsed ? ">" : "v"}
+          </Button>
+        )}
         <span>{node.name}</span>
         <Buttons>
           <Button size="20px" margin="0 2px" onClick={removeLayer}>
@@ -40,9 +50,11 @@ const Layer = ({ node }) => {
           </Button>
         </Buttons>
       </Main>
-      <Children color={node.color}>
-        {node.children && node.children.map(n => <Layer node={n} />)}
-      </Children>
+      {!collapsed && (
+        <Children color={node.color}>
+          {node.children && node.children.map(n => <Layer node={n} />)}
+        </Children>
+      )}
     </Wrapper>
   );
 };
